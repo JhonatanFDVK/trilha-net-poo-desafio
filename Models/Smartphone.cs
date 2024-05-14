58aaf56a105f46ed2578176fca71b4db1ba25b6d
Jhonatan Silva <jhonatanfdvk@gmail.com>
2024-05-14 20:22:52 -0300

    Implementada as propriedades que faltavam e passado os parâmetros do construtor para as propriedades

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private String _modelo;
+        private String _IMEI;
+        private int _memoria;
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, String modelo, String IMEI, int memoria)
         {
             Numero = numero;
+            _modelo = modelo;
+            _IMEI = IMEI;
+            _memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
